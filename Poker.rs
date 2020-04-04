297eea2d1e266c0605bdff154c730ee176022afc
Nahzilian <50055124+Nahzilian@users.noreply.github.com>
2020-04-04 01:10:46 -0400

    Fixing haspair

--- Poker.rs
+++ Poker.rs
@@ -14,29 +14,75 @@ fn deal(arr:[u32;10]) -> String{
     hand1.sort();
     hand2.sort();
 
-    println!("{:?}",hand1);
-    println!("{:?}",hand2);
-
+    //winner(hand1,hand2);
     buf.push('c');
+
     return buf;
 }
 
-fn winner(n:i32) -> i32{
-    return 1
+fn winner(arr1:[u32;5],arr2:[u32;5]){
+    let mut winner = 0;
+    if highcard(arr1) > highcard(arr2){
+        winner = 1;
+    }else{
+        winner = 2;
+    }
+    println!("In progress");
 }
 // Winner function:
 //High card
-fn highcard(arr:[u32;5]){
+fn highcard(arr:[u32;5]) -> u32{
+    let mut result = 0;
     for (i,item) in arr.iter().enumerate(){
-        if i%2 == 0{
-            hand1.push(*item);
+        if (*item == 1) || (*item == 14) || (*item == 27) || (*item == 40){
+            if result < *item{result = *item;}
+            result = *item
         }else{
-            hand2.push(*item);
+            if result < *item && (result !=1) && (*item != 40) && (*item != 14) && (*item != 27) {
+                result = *item;
+            }
         }        
     }
+    println!("{:?}",arr);
+    return result;
+}
+//Has pair
+/*
+    loop through the whole code and find the pairs, return the vector for all the pairs.
+*/
+
+fn hasPair(arr:[u32;5]) -> Vec<u32>{
+    let mut result = Vec::new();
+    let mut temp = 0;
+    for (i,item1) in arr.iter().enumerate(){
+        temp = *item1;
+        for (j,item2) in arr.iter().enumerate(){
+            if (temp + 13 == *item2) || (temp + 13*2 == *item2) || (temp + 13*3 == *item2)|| (temp + 13*4 == *item2){
+                result.push(*item1);
+                result.push(*item2);
+            }
+        }
+    }
+    if result.is_empty{
+        
+    }
+    return result;
 }
 
+//has two pair
+//fn hasTwoPair(arr:[u32;5]){
+    //let mut result = Vec::new();
+    //let mut count = 0;
+    //for (i,item) in arr.iter().enumerate(){
+        
+    //}
+
+
+  
+//}
 fn main(){
     let  a = deal([42,2,3,4,5,6,7,8,9,9]);
     println!("{}",a);
+    println!("{:?}",highcard([2,3,4,5,6]));
+    println!("{:?}",hasPair([1,2,14,15,9]));
 }
--- main.rs
+++ main.rs
@@ -18,9 +18,17 @@ fn main(){
     let mut array = [0,0,0,0,0];
     array[0] = 1;
     println!("{}",array[0]);
-
+    array.push(1);
+    println!("{:?}",array);
+    let mut arr = Vec::new();
+    test2(arr);
 }
 
 fn test(n:i32) ->i32{
     n+1
+}
+fn test2(arr:u32){
+    let v = vec!arr;
+    arr.push(1);
+    println!("{:?}",arr);
 }
